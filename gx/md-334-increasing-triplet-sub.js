bc21dd6f4e09770362b1a3496da4353ebc8eb813
wvha <Wvha17@gmail.com>
2020-01-31 11:40:44 -0800

    added simpler solution

--- gx/md-334-increasing-triplet-sub.js
+++ gx/md-334-increasing-triplet-sub.js
@@ -32,3 +32,19 @@ var increasingTriplet = function(nums) {
     return false
 };
 
+// shorter solution
+var increasingTriplet = function(nums) {
+    let min = nums[0]
+    let secondMinUpdatedAfterMin = Infinity
+
+    for (let val of nums) {
+        if (val <= min) {
+            min = val
+        } else if (val <= secondMinUpdatedAfterMin) {
+            secondMinUpdatedAfterMin = val
+        } else { // min < secondMinUpdatedAfterMin < val
+            return true 
+        }
+    }
+    return false
+}
