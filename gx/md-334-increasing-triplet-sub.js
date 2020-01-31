3c4571c68856bd2c73253a9c81d7d1184fb72691
wvha <Wvha17@gmail.com>
2020-01-31 11:38:22 -0800

    completed first medium problem

---
+++ gx/md-334-increasing-triplet-sub.js
@@ -0,0 +1,34 @@
+var increasingTriplet = function(nums) {
+    
+    let count = 1
+    let lowest = nums[0]
+    let current = nums[0]
+    
+    for (let i = 1; i < nums.length; i++) {
+        
+        // if nums[i] is greater, add to count, and set new lowest
+        if (nums[i] > current) {
+            current = nums[i]
+            count++
+            if (count === 3) return true
+        }
+        
+        // if nums[i] is smaller, restart the count and lowest
+        else if (nums[i] < lowest) {
+            if (nums[i+1] > nums[i] && nums[i+1] <= current) { // guarantees that count at nums[i+1] will be > 1
+                lowest = nums[i]
+                current = nums[i]
+                count = 1
+            }
+        }
+        
+        // if there's a next possible number < current 
+        if (nums[i] < current && nums[i] > lowest) {
+            count = 2
+            current = nums[i]
+        }
+    }
+
+    return false
+};
+
